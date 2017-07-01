fba802918aab24babc2fb99702ced1bd55cb69f0
Arvind Prakash <arvindprksh@gmail.com>
2017-07-02 00:57:53 +0530

    second commit

---
+++ action_page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php stuff</title>
+</head>
+<body>
+HI<?php echo $_POST["firstName"];?>
+HELLO<?php echo $_POST["lastName"];?>
+BRO <?php echo $_POST["email"];?>
+SDFDSF<?php echo $_POST["mobileNumber"];?>
+
+</body>
+</html>						
