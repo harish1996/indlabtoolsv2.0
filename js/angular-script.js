274ee79970498d4d77f58f276925190bd707d77f
harish1996 <harishganesan96@gmail.com>
2017-07-09 00:29:01 +0530

    New look product page

---
+++ js/angular-script.js
@@ -0,0 +1,30 @@
+	var app=angular.module('app',[]);
+	app.controller('controller',function($scope){
+		$scope.unit_name=["Incubator","Lab Oven","Muffle Furnace","Tray Drier","Vacuum Oven","Deep Freezer","Walk in Chamber","pH Meter","Conductivity Meter","Analytical Balance","Spectrophotometer","Centrifuge","Environmental Chamber","Laminar Air Flow Cabinet","Rabbit Cage/Mice Cage","Rat/Mice Cage","Animal Rack","Micro Pipette","Semi Auto Analyzer","Microscopes","Motors","Jaw Crusher","Fume Hoods","Profile Projector","Equipment for Histopathology / Microbiology"]
+		$scope.content=["Incubator – Bacteriological Incubator to work on above ambient to 60°C –Available in various sizes/types",
+		"Lab Oven- For Sterilization/Drying samples/Glasswares. Max Temperature 250°C – Available in various sizes/types and with high temperature also.",
+		"Muffle Furnace - Max Temperature 930°C and 1200°C. Also available High Temperature Furnaces, Tube Furnaces etc.",
+		"Tray Drier - Suitable for a Food Processing. Chemical/Pharmaceutical Industries. No. of Trays - 12, 24, 48 and 96. – A custom Built equipment",
+		"Vacuum Oven - To dry samples under Vacuum conditions. Working Temperature 50°C to 200°C - Available in various sizes/types",
+		"Deep Freezer – Low temperature Cabinets Ranges : -20°C, -40°C, -70°C, and -80°C. Available in Vertical and Horizontal versions. Custom built design can also be made available.",
+		"Walk in Chamber - For storing samples, food products, Agricultural produces in cold / below room temperature conditions. 100% custom built design",
+		"pH Meter – For analyzing pH/mV value – Economy as well as Hi-End ranges available",
+		"Conductivity Meter - For analyzing conductivity/TDS value etc. – Economy as well as Hi-End ranges available",
+		"Analytical Balance - Micro to Macro Ranges with high accuracy. Table top/Portable/Battery operated. Weighing range from 80g to 220g in micro ranges and 1 Kg to 1000Kgs for Lab purposes also IR moisture balances available.",
+		"Spectrophotometer - Visible range, UV-Visible range, FTIR, Fluorometer etc.",
+		"Centrifuge - Clinical, Research, Hi-Speed, Cooling, Oil Testing Centrifuges, Blood Bank Centrifuges, Dedicated version for Biotech etc.",
+		"Environmental Chamber - For environmental study in Below Ambient and Above Ambient conditions, Suitable for Plant growth, Seed Germinations etc.",
+		"Laminar Air Flow Cabinet - Horizontal and Vertical versions, Bio Safety cabinets, Glove Boxes , Airshower, Clean room equipment etc.",
+		"Rabbit Cage - Suitable for Drug testing, Pharmaceutical /Cosmetic Industries in different versions as per customer need. Made of SS 304 material",
+		"Made out of Autoclavable	 Polypropylene material in different sizes",
+		"Animal Rack - Suitable for stacking Rabbit cages, Mice/Rat cages with wheel arrangement – a custom built product",
+		"Micro Pipette - Liquid handling system in micro to macro volumes – available in single and multi channels",
+		"Semi Auto Analyzer, Elisa Reader, Elisa Washer, Chemistry Analyser, Electrophoresis, PCR, Fermentor, Gel Documentation Systems etc.",
+		"Microscopes – Basic models to High end models – in Monocular, Binocular and Trinocular versions, with optional attachments, Inverted Tissue culture etc. for various research applications. Both Indian and imported make",
+		"Motors - AC/DC motors, AC motors, DC motors in Single and Three phases, Geared motors for various applicatio</ns, with Drive etc. <br>Ranging from 1/50 HP to 50 HP – Suitable for continuous operation",
+		"RAYMOND MINI MILL (HAMMER MILL) Feed size (a)50mm, (b) 3 to 6 mm Output size 212 MicronsBench mounting with replaceable fittings. Fabricated mild steel construction fitted with forged steel hammers.	Supplied with complete feed hoppers Fitted with suitaable 3 phase motor with belt guard and starter",
+		"Made out of Plywood or Mild Steel or Stainless Steel construction and three sides will be lined with heat and acid resistant FRP. Available in Floor Models, Table top models, with mount on blower or External blower with Dynamically balanced FRP impeller, as per site requirement. Work table made of 18/20mm thick Black Granite or Stainless Steel or Ceramic top. Gas tap connection with control knob on front panels, provision for additional power fittings etc. as per customer needs",
+		"For accurate measurement of Stamped Parts, Tools, seals and gaskets, Turned Parts, Profile, Punches, Dies, shafts etc. Availble in both Vertical and Horizontal Models with Screen Size 300 mm to 1000 mm.Integrated Edge Sensor with integration of a high resolution CCD Camera in the Optical Path makes for fast Measurements and documentation.Read out options available are software and DRO’s with full geometric functions",
+		"Tissue Floatation Bath, Slide Warming Table, Paraffin Dispenser and Water Bath Thermostatic .. Frozen Tissues Pre-Analytical Tools Rapid Tissue Processing Macro Digital and Workstation. Autoclaves, ULTRA CRYO baths, Freeze Dryers,Anearobic jars and workstation"]
+		$scope.a=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+	});
